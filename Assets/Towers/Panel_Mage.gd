4c0b903d54332771abf71d12a2c2382d44186e51
JFraser99 <jf0907@hotmail.co.uk>
2023-10-30 18:38:06 +0000

    Fixed bugs with tower placement

--- Assets/Towers/Panel_Mage.gd
+++ Assets/Towers/Panel_Mage.gd
@@ -5,21 +5,33 @@ extends Panel
 var currTile
 @onready var tempTowersPath = get_tree().get_root().get_node("Main/TempTowers")
 @onready var towersPath = get_tree().get_root().get_node("Main/Towers")
+var canAddAnother: bool = true
 
 func _on_gui_input(event):
-	var tempTower = tower.instantiate()
-	if event is InputEventMouseButton and event.button_mask == 1:
-		# Left Click Down
-		tempTowersPath.add_child(tempTower)
-		tempTowersPath.get_child(0).global_position = event.global_position
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-	elif event is InputEventMouseMotion and event.button_mask == 1:
+	if event is InputEventMouseMotion:
 		# Left Click Down Drag
-		tempTowersPath.get_child(0).global_position = event.global_position
-	elif event is InputEventMouseButton and event.button_mask == 0:
-		# Left Click Up
-		tempTowersPath.get_child(0).queue_free()
+		if tempTowersPath.get_child_count() > 0:
+			tempTowersPath.get_child(0).global_position = event.global_position
+	elif event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.button_mask == 0 and tempTowersPath.get_child_count() > 0:
+				# Left Click Up
+				tempTowersPath.get_child(0).queue_free()
+				
+				var tempTower = tower.instantiate()
+				towersPath.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("Area").hide()
+				canAddAnother = true
+			elif canAddAnother and event.button_mask == 1:
+				# Left Click Down
+				var tempTower = tower.instantiate()
+				tempTowersPath.add_child(tempTower)
+				tempTowersPath.get_child(0).global_position = event.global_position
+				tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+				canAddAnother = false
+		else:
+			if tempTowersPath.get_child_count() > 0:
+				tempTowersPath.get_child(0).queue_free()
+				canAddAnother = true
 		
-		towersPath.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		tempTower.get_node("DetectionArea").hide()
